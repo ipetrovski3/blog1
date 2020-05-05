10dbba9774dcfe125f236fa48460eccdf89ed72b
Igor Petrovski <ipetrovski3@gmail.com>
2020-05-05 21:29:49 +0200

    Added Create New Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,19 @@ class ArticlesController < ApplicationController
     id = params[:id]
     @article = Article.find(id)
   end
+
+  def new
+  end
+
+  def create
+    article = Article.create(article_params)
+
+    redirect_to article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
