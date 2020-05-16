c28f72c5bd5379ff94e482b078021dbfb36664b4
Igor Petrovski <ipetrovski3@gmail.com>
2020-05-16 20:05:48 +0200

    Bulma CSS implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   # get 'articles/index'
-  get '/articles', to: 'articles#index'
-  post '/articles', to: 'articles#create'
-  get '/articles/new', to: 'articles#new', as: :new_article
-  get '/articles/:id', to: 'articles#show', as: :article
-  get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
-  patch '/articles/:id', to: 'articles#update'
-  delete '/articles/:id', to: 'articles#destroy'
+  resources :articles
 
 
   root to: 'articles#index'
